a0b036737f4b4edb247cf86f802b5c6d13d21725
Jonasz Koryl <jonasz@koryl.name>
2025-11-26 21:25:17 +0000

    Zmiana na petle while

--- main.cpp
+++ main.cpp
@@ -4,13 +4,12 @@ int main() {
 char jakiejDanuty[] = "twojastara";
 std::cout << "Ej nie widziales moze Danuty" << std::endl;
 std::cin >> jakiejDanuty;
-if (jakiejDanuty == "jakiej Danuty"){
-std::cout <<"Tej do kradnie blache z huty"<<std::endl;
+while (jakiejDanuty =! "jakiej_Danuty"){
+std::cout << "To nie jest pytanie na moja odpowiedz!"<<std::endl;
+std::cin >> jakiejDanuty;
 }
-else {
-std::cout<<"To nie jest pytanie na moja odpowiedz"<<std::endl;
-std::cin >>jakiejDanuty;
-}
+std<<cout << "Tej co kradnie blache z huty";
+
 return 0;
 std::cout<<"Witam ;)";
 }
