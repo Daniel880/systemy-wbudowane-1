b71ac2020d73d9afbb59a13f6a110c7a9108d648
Jonasz Koryl <jonasz@koryl.name>
2025-11-26 21:08:10 +0000

    Wprowadzono smieszne.jpg

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
 int main() {
-std::cout << "Hello world" << std::endl;
+string jakiejDanuty;
+std::cout << "Ej nie widziales moze Danuty" << std::endl;
+std::cin >> jakiejDanuty;
+if (jakiejDanuty == "jakiej Danuty?"){
+std::cout <<"Tej do kradnie blache z huty"<<std::endl;
+}
+else {
+std::cout<<"To nie jest pytanie na moja odpowiedz"<<std::endl;
+std::cin >>jakiejDanuty;
 return 0;
 }
