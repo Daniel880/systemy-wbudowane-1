51dc159b5609f577ce7b0ff14e4cab8976cfb9f0
Marcin Kociolek <marcin@kociolek.com>
2025-11-26 15:13:21 +0000

    Opracowano komende MarcinKociolek

--- commands/MarcinKociolek_command.cpp
+++ commands/MarcinKociolek_command.cpp
@@ -1,1 +1,21 @@
+#include "command.h"
+#include <iostream>
 
+class MarcinKociolekCommand : public Command {
+public:
+  void execute() override {
+    std::cout << "=== Marcin Kociolek Command ===" << std::endl;
+    std::cout << "Pamietaj:" << std::endl;
+
+    std::cout << "Nie pozdrawiam." << std::endl;
+
+    std::cout << "\n=============================" << std::endl;
+  }
+
+  std::string getDescription() const override {
+    return "Powie Ci mile slowo";
+  }
+};
+
+// Global instance - will be registered in main.cpp
+MarcinKociolekCommand marcinKociolekCommand;
