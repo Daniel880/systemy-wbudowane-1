738b183d8bad82fadd9a5ac55965e153f75d10cc
root <piotr@goinski.com>
2025-12-17 20:59:23 +0000

    Dodalem maina z x

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+
+
+
+int main() { 
+
+ int x{}; 
+
+
+std::cout<< " Zgadne twoja liczbe (wpisz ja na klawiaturze teraz) \n";
+std::cin>>x;
+std::cout<<" Czy twoja liczba to  "<<x;
+
+
+
+
+	return 0; 
+
+
+
+} 
+
+
+
+
+
