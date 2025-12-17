f5d8b2bfd90e98864bc6ed34e48689a504dbb93b
root <piotr@goinski.com>
2025-12-17 21:47:37 +0000

    dodalem moje ID

---
+++ commands/PiotrGoinski_command.cpp
@@ -0,0 +1,17 @@
+#include "command.h"
+#include <iostream>
+
+
+	class PiotrGoinskiCommand : public Command { 
+	public: 
+	  void execute() override { 
+	    std::cout << "=== Piotr Goinski Command ===" << std::endl; 
+	    std::cout << "Student ID: 42069 << std::endl; 
+	}	
+	
+	std::string getDescription() cons override { 
+	  return "Zwraca opis studenta Piotra Goinskiego";
+	}
+	};
+	
+	PiotrGoinskiCommand piotrGoinskiCommand;
